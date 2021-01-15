2b3594d7f41d3c630f64cc4df23c8d82fbf6beda
Tsvetelin Yakimov <ceci2205@abv.bg>
2021-01-15 18:53:20 +0200

    Make facebook chat language the same as the selected website language.

--- public/style/js/facebook-chat.js
+++ public/style/js/facebook-chat.js
@@ -1,22 +1,28 @@
-var FacebookChatManager = (function (env, greetingMessage) {
+var FacebookChatManager = (function (env, lang, greetingMessage) {
+    var facebookSdkByLang = {
+        bg: 'https://connect.facebook.net/bg_BG/sdk/xfbml.customerchat.js',
+        en: 'https://connect.facebook.net/en_US/sdk/xfbml.customerchat.js'
+    }
+
     var facebookInstance = null;
 
     var facebookMessageButtonContainer = $('.facebook-message-button-container');
     var facebookMessageButton = $('.facebook-message-button');
 
-    function injectFacebookSdk(d, s, id) {
+    function injectFacebookSdk(d, s, id, lang) {
         var js, fjs = d.getElementsByTagName(s)[0];
         if (d.getElementById(id)) return;
         js = d.createElement(s);
         js.id = id;
-        js.src = 'https://connect.facebook.net/en_US/sdk/xfbml.customerchat.js';
+        js.src = facebookSdkByLang[lang];
         fjs.parentNode.insertBefore(js, fjs);
     }
 
     /**
+     * @param lang currently selected language
      * @param callback returns FB object when it loads
      */
-    function loadFacebookSdk(callback) {
+    function loadFacebookSdk(lang, callback) {
         window.fbAsyncInit = function () {
             facebookInstance = FB;
             facebookInstance.init({
@@ -27,7 +33,7 @@ var FacebookChatManager = (function (env, greetingMessage) {
             facebookInstance.XFBML.parse(document, callback);
         };
 
-        injectFacebookSdk(document, 'script', 'facebook-jssdk');
+        injectFacebookSdk(document, 'script', 'facebook-jssdk', lang);
     }
 
     function initEvents() {
@@ -36,17 +42,18 @@ var FacebookChatManager = (function (env, greetingMessage) {
         });
     }
 
-    function init(env, greetingMessage) {
+    function init(env, lang, greetingMessage) {
         if (env !== 'prod') {
             console.warn('Facebook chat only works on prod!');
             return;
         }
 
-        loadFacebookSdk(function () {
+        loadFacebookSdk(lang, function () {
             facebookInstance.CustomerChat.hideDialog();
             facebookInstance.CustomerChat.update({
                 logged_in_greeting: greetingMessage,
                 logged_out_greeting: greetingMessage,
+                ref: 'ref_'
             });
 
             initEvents();
@@ -54,9 +61,9 @@ var FacebookChatManager = (function (env, greetingMessage) {
         });
     }
 
-    init(env, greetingMessage);
+    init(env, lang, greetingMessage);
 
     return {};
 });
 
-var facebookChatManager = new FacebookChatManager(ENV, CONTACT_US_MESSAGE);
+var facebookChatManager = new FacebookChatManager(ENV, SELECTED_LANGUAGE, CONTACT_US_MESSAGE);
